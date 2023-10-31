bda19d71b40e7c992d1457b3f7f5374fea999f7b
camerongineer <alphaomegabeta@gmail.com>
2023-10-31 11:01:07 -0700

    Add useSemiPersistentState hook; Remove unused data and listUtils

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import TodoList from "./TodoList";
 import AddTodoForm from "./AddTodoForm";
 
+const useSemiPersistentState = (key, initialState) => {
+    const [value, setValue] = useState(
+        localStorage.hasOwnProperty(key)
+            ? JSON.parse(localStorage.getItem(key))
+            : initialState
+    );
+    
+    useEffect(() => {
+        localStorage.setItem(key, JSON.stringify(value));
+    }, [key, value]);
+    
+    return [value, setValue];
+};
+
 const App = () => {
-    const [todoList, setTodoList] = useState([]);
+    const [todoList, setTodoList] = useSemiPersistentState("savedTodoList", []);
     const addTodo = (newTodo) => setTodoList(prevTodoList => [...prevTodoList, newTodo]);
     
     return (
