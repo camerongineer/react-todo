3cc763995b78d8a9f9c1a5d83b6fd97ea239f002
camerongineer <alphaomegabeta@gmail.com>
2023-11-02 16:01:06 -0700

    Change conditional operator

--- src/App.js
+++ src/App.js
@@ -3,11 +3,7 @@ import TodoList from "./TodoList";
 import AddTodoForm from "./AddTodoForm";
 
 const useSemiPersistentState = (key, initialState) => {
-    const [value, setValue] = useState(
-        localStorage.hasOwnProperty(key)
-            ? JSON.parse(localStorage.getItem(key))
-            : initialState
-    );
+    const [value, setValue] = useState(JSON.parse(localStorage.getItem(key)) ?? initialState);
     
     useEffect(() => {
         localStorage.setItem(key, JSON.stringify(value));
