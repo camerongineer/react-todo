cfefad8504c1c81136c31d3cc8a049874bef81c6
camerongineer <alphaomegabeta@gmail.com>
2023-11-03 09:40:15 -0700

    Change div to fragment

--- src/App.js
+++ src/App.js
@@ -17,13 +17,13 @@ const App = () => {
     const addTodo = (newTodo) => setTodoList(prevTodoList => [...prevTodoList, newTodo]);
     
     return (
-        <div>
+        <>
             <h1>Todo List</h1>
             <hr/>
             <AddTodoForm onAddToDo={addTodo}/>
             <TodoList todoList={todoList}/>
             <hr/>
-        </div>
+        </>
     );
 };
 
