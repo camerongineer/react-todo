8207ae28f9f1b7f689568729a1352fcaf380fae5
Cameron Marr <alphaomegabeta@gmail.com>
2023-11-16 22:11:42 -0800

    Remove useSemiPersistentState hook; Add useEffect mimicking API call to retrieve localStorage data

--- src/App.js
+++ src/App.js
@@ -2,27 +2,39 @@ import React, { useEffect, useState } from "react";
 import TodoList from "./TodoList";
 import AddTodoForm from "./AddTodoForm";
 
-const useSemiPersistentState = (key, initialState) => {
-    const [value, setValue] = useState(JSON.parse(localStorage.getItem(key)) ?? initialState);
-    
-    useEffect(() => {
-        localStorage.setItem(key, JSON.stringify(value));
-    }, [key, value]);
-    
-    return [value, setValue];
-};
-
 const App = () => {
-    const [todoList, setTodoList] = useSemiPersistentState("savedTodoList", []);
+    const [todoList, setTodoList] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
     const addTodo = (newTodo) => setTodoList(prevTodoList => [...prevTodoList, newTodo]);
     const removeTodo = (id) => setTodoList(prevTodoList => prevTodoList.filter(item => item.id !== id));
+    
+    useEffect(() => {
+        new Promise(resolve => {
+            setTimeout(() => {
+                resolve({ data: { todoList: JSON.parse(localStorage.getItem("todoList")) ?? [] } });
+            }, 2000);
+        }).then(result => {
+            setTodoList(result.data.todoList);
+            setIsLoading(false);
+        });
+    }, []);
+    
+    useEffect(() => {
+        if (!isLoading) {
+            localStorage.setItem("todoList", JSON.stringify(todoList));
+        }
+    }, [todoList]);
     
     return (
         <>
             <h1>Todo List</h1>
             <hr/>
             <AddTodoForm onAddTodo={addTodo}/>
-            <TodoList todoList={todoList} onRemoveTodo={removeTodo}/>
+            {isLoading ? (
+                <p>Loading...</p>
+            ) : (
+                <TodoList todoList={todoList} onRemoveTodo={removeTodo}/>
+            )}
             <hr/>
         </>
     );
