dd60212e425e8caa17d76d5374bdafd94e10a85a
Cameron Marr <alphaomegabeta@gmail.com>
2023-11-29 16:07:49 -0800

    Add todoReducer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useReducer, useState } from "react";
 import TodoList from "./TodoList";
 import AddTodoForm from "./AddTodoForm";
+import todoReducer from "./todoReducer";
 
 const App = () => {
-    const [todoList, setTodoList] = useState([]);
+    // const [todoList, setTodoList] = useState([]); <== OLD WAY
+    const [todoList, dispatch] = useReducer(todoReducer, []); // <=== NEW WAY
+    
     const [isLoading, setIsLoading] = useState(true);
-    const addTodo = (newTodo) => setTodoList(prevTodoList => [...prevTodoList, newTodo]);
-    const removeTodo = (id) => setTodoList(prevTodoList => prevTodoList.filter(item => item.id !== id));
+    
+    //const addTodo = (newTodo) => setTodoList(prevTodoList => [...prevTodoList, newTodo]); <== OLD WAY
+    const addTodo = (newTodo) => dispatch({type: "ADD_TODO", payload: newTodo}); //<== NEW WAY
+    
+    // const removeTodo = (id) => setTodoList(prevTodoList => prevTodoList.filter(item => item.id !== id));
+    const removeTodo = (id) => dispatch({type: "REMOVE_TODO", payload: id}); // NEW WAY
     
     useEffect(() => {
         new Promise(resolve => {
@@ -14,7 +21,10 @@ const App = () => {
                 resolve({ data: { todoList: JSON.parse(localStorage.getItem("todoList")) ?? [] } });
             }, 2000);
         }).then(result => {
-            setTodoList(result.data.todoList);
+            
+            // setTodoList(result.data.todoList); <== OLD WAY
+            dispatch({ type: "SET_TODOS", payload: result.data.todoList ?? [] }); // <=== NEW WAY
+            
             setIsLoading(false);
         });
     }, []);
