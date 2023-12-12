c37b4c25c66aabb2bfc045bc41c76302d9e19e46
Cameron Marr <alphaomegabeta@gmail.com>
2023-12-11 16:36:59 -0800

    Add axios with interceptors to set loading state

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import axios from "axios";
 import TodoList from "./TodoList";
 import AddTodoForm from "./AddTodoForm";
 
@@ -7,34 +8,41 @@ const BASE_ID = process.env.REACT_APP_AIRTABLE_BASE_ID;
 const TABLE_NAME = process.env.REACT_APP_TABLE_NAME;
 const API_TOKEN = process.env.REACT_APP_AIRTABLE_API_TOKEN;
 // Sorts todos by lastModifiedTime if this entry exists as field type
-const SORT_BY_LAST_MODIFIED_TIME = ""; //"?sort%5B0%5D%5Bfield%5D=lastModifiedTime&sort%5B0%5D%5Bdirection%5D=asc";
+const SORT_BY_LAST_MODIFIED_TIME = "?sort%5B0%5D%5Bfield%5D=lastModifiedTime&sort%5B0%5D%5Bdirection%5D=asc";
 
 const AIRTABLE_URL = `${BASE_URL}/${BASE_ID}/${TABLE_NAME}`;
 
 const fetchAirtableData = async ({ method, url, body }) => {
-    const response = await fetch(`${AIRTABLE_URL}${url ?? ""}`, {
-        method,
-        headers: {
-            "Authorization": `Bearer ${API_TOKEN}`,
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(body)
-    });
+    const headers = {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${API_TOKEN}`
+    };
     
-    if (!response.ok) {
-        return Promise.reject(`Error: ${response.status}`);
+    let response;
+    switch (method) {
+        case "POST":
+            response = await axios.post(`${AIRTABLE_URL}${url ?? ""}`, body, { headers });
+            break;
+        case "GET":
+            response = await axios.get(`${AIRTABLE_URL}${url ?? ""}`, { headers });
+            break;
+        case "DELETE":
+            response = await axios.delete(`${AIRTABLE_URL}${url ?? ""}`, { headers });
+            break;
+        default:
+            throw new Error("Invalid method type.");
     }
     
-    if (response.headers.get("Content-Type").includes("application/json")) {
-        return await response.json();
+    if (!response || response.status !== 200) {
+        throw new Error(`Error: ${response ? response.status : "Unknown"}`);
     }
     
-    return null;
+    return response.data;
 };
 
 const App = () => {
     const [todoList, setTodoList] = useState([]);
-    const [isLoading, setIsLoading] = useState(true);
+    const [loading, setLoading] = useState(true);
     
     const addTodo = async (newTodo) => {
         const airtableData = {
@@ -60,16 +68,51 @@ const App = () => {
     };
     
     useEffect(() => {
+        // Add a request interceptor
+        const requestInterceptor = axios.interceptors.request.use(
+            (config) => {
+                // Show loading indicator
+                setLoading(true);
+                return config;
+            },
+            (error) => {
+                // Hide loading indicator on request error
+                setLoading(false);
+                return Promise.reject(error);
+            }
+        );
+        
+        // Add a response interceptor
+        const responseInterceptor = axios.interceptors.response.use(
+            (response) => {
+                // Hide loading indicator on successful response
+                setLoading(false);
+                return response;
+            },
+            (error) => {
+                // Hide loading indicator on response error
+                setLoading(false);
+                return Promise.reject(error);
+            }
+        );
+        
+        // Cleanup the interceptors when the component is unmounted
+        return () => {
+            axios.interceptors.request.eject(requestInterceptor);
+            axios.interceptors.response.eject(responseInterceptor);
+        };
+    }, []); // Run this effect only once on component mount
+    
+    useEffect(() => {
         const loadData = async () => {
-            setIsLoading(true);
             await loadTodos();
-            setIsLoading(false);
         };
         loadData();
     }, []);
     
     const loadTodos = async () => {
         try {
+            debugger
             const response = await fetchAirtableData({ method: "GET", url: SORT_BY_LAST_MODIFIED_TIME });
             
             const todosFromAPI = await response;
@@ -91,7 +134,7 @@ const App = () => {
             <h1>Todo List</h1>
             <hr/>
             <AddTodoForm onAddTodo={addTodo}/>
-            {isLoading ? (
+            {loading ? (
                 <p>Loading...</p>
             ) : (
                 <TodoList todoList={todoList} onRemoveTodo={removeTodo}/>
