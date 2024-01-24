32e5c61b28fd7369aba7d3896e99eb7c5f6c4d9a
Cameron Marr <alphaomegabeta@gmail.com>
2024-01-24 12:42:21 -0800

    Add InputWithLabel prop types

--- src/components/InputWithLabel.js
+++ src/components/InputWithLabel.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useRef } from "react";
+import PropTypes from "prop-types";
 
 const InputWithLabel = ({
     inputId,
@@ -22,14 +23,24 @@ const InputWithLabel = ({
                 {children}
             </label>
             &nbsp;
-            <input ref={inputRef}
-                   id={inputId}
-                   type={type}
-                   value={inputValue}
-                   onChange={onInputChange}
+            <input
+                ref={inputRef}
+                id={inputId}
+                type={type}
+                value={inputValue}
+                onChange={onInputChange}
             />
         </>
     );
+};
+
+InputWithLabel.propTypes = {
+    inputId: PropTypes.string.isRequired,
+    inputValue: PropTypes.string.isRequired,
+    type: PropTypes.string,
+    onInputChange: PropTypes.func.isRequired,
+    isFocused: PropTypes.bool.isRequired,
+    children: PropTypes.string
 };
 
 export default InputWithLabel;
