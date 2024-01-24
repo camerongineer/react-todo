ab589c386c30d2eb5be81b1e48156d39005d8cf3
Cameron Marr <alphaomegabeta@gmail.com>
2024-01-24 12:44:01 -0800

    Add TodoList prop types

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,5 +1,6 @@
 import React from "react";
 import TodoListItem from "./TodoListItem";
+import PropTypes from "prop-types";
 
 const TodoList = ({ todoList, onRemoveTodo }) => (
     <ul>{todoList.map(listItem =>
@@ -9,5 +10,10 @@ const TodoList = ({ todoList, onRemoveTodo }) => (
             onRemoveClicked={onRemoveTodo}
         />)}
     </ul>);
+
+TodoList.propTypes = {
+    todoList: PropTypes.array.isRequired,
+    onRemoveTodo: PropTypes.func.isRequired,
+};
 
 export default TodoList;
