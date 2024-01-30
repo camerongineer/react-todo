a3d08c9000cf6218d66f0df8329a8966ac75c6d9
Cameron Marr <alphaomegabeta@gmail.com>
2024-01-30 09:56:16 -0800

    Add sorting options for list

--- src/components/TodoContainer.jsx
+++ src/components/TodoContainer.jsx
@@ -1,16 +1,19 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 import styles from "../styles/TodoContainer.module.css";
 import AddTodoForm from "./AddTodoForm";
 import Loading from "./Loading";
 import TodoList from "./TodoList";
+import { sortByField } from "../utils";
+import SortBox from "./SortBox";
 
 const BASE_URL = "https://api.airtable.com/v0";
 const BASE_ID = process.env.REACT_APP_AIRTABLE_BASE_ID;
 const TABLE_NAME = process.env.REACT_APP_TABLE_NAME;
 const API_TOKEN = process.env.REACT_APP_AIRTABLE_API_TOKEN;
-// Sorts todos by lastModifiedTime if this entry exists as field type
-const SORT_BY_LAST_MODIFIED_TIME = ""; //"?sort%5B0%5D%5Bfield%5D=lastModifiedTime&sort%5B0%5D%5Bdirection%5D=asc";
+const GRID_VIEW = "view=Grid view";
+const SORT_BY_TITLE = "sort[0][field]=title&sort[0][direction]=asc";
+const SORT_BY_LAST_MODIFIED_TIME = "sort[0][field]=lastModifiedTime&sort[0][direction]=asc";
 
 const AIRTABLE_URL = `${BASE_URL}/${BASE_ID}/${TABLE_NAME}`;
 
@@ -19,14 +22,14 @@ const fetchAirtableData = async ({ method, url, body }) => {
         "Content-Type": "application/json",
         Authorization: `Bearer ${API_TOKEN}`
     };
-
+    
     const axiosConfig = {
         method: method,
         url: `${AIRTABLE_URL}${url ?? ""}`,
         headers: headers,
         ...(body ? { data: body } : {})
     };
-
+    
     try {
         const response = await axios(axiosConfig);
         return response.data;
@@ -38,7 +41,9 @@ const fetchAirtableData = async ({ method, url, body }) => {
 const TodoContainer = () => {
     const [todoList, setTodoList] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
-
+    const [sortBy, setSortBy] = useState("lastModifiedTime");
+    const [isReversed, setIsReversed] = useState(true);
+    
     const addTodo = async (newTodo) => {
         const airtableData = {
             fields: {
@@ -52,7 +57,7 @@ const TodoContainer = () => {
         }
         await loadTodos();
     };
-
+    
     const removeTodo = async (id) => {
         try {
             await fetchAirtableData({ method: "DELETE", url: `/${id}` });
@@ -61,33 +66,46 @@ const TodoContainer = () => {
         }
         await loadTodos();
     };
-
-    useEffect(() => {
-        const loadData = async () => {
-            setIsLoading(true);
-            await loadTodos();
-            setIsLoading(false);
-        };
-        loadData();
-    }, []);
-
+    
     const loadTodos = async () => {
         try {
-            const response = await fetchAirtableData({ method: "GET", url: SORT_BY_LAST_MODIFIED_TIME });
-
+            const response = await fetchAirtableData({ method: "GET", url: `?${GRID_VIEW}` });
+            
             const todosFromAPI = await response;
-
-            setTodoList(todosFromAPI.records.map(todo => {
-                return {
-                    id: todo.id,
-                    title: todo.fields.title
-                };
-            }));
+            
+            setTodoList(
+                sortByField(
+                    todosFromAPI.records.map(todo => {
+                        return {
+                            id: todo.id,
+                            title: todo.fields.title,
+                            lastModifiedTime: todo.fields.lastModifiedTime
+                        };
+                    }),
+                    sortBy,
+                    isReversed
+                ));
         } catch (error) {
             console.log(error.message);
             setTodoList([]);
         }
     };
+    
+    useEffect(() => {
+        (async () => {
+            setIsLoading(true);
+            await loadTodos();
+            setIsLoading(false);
+        })();
+    }, []);
+    
+    useEffect(() => {
+        setTodoList(prevState => sortByField(prevState, sortBy, isReversed));
+    }, [isReversed, sortBy]);
+    
+    const handleIsReversedChange = () => setIsReversed(!isReversed);
+    const handleSortFieldChange = (event) => setSortBy(event.target.value);
+    
     return (
         <div className={styles.container}>
             <div className={styles.App}>
@@ -95,10 +113,18 @@ const TodoContainer = () => {
                 {isLoading ? (
                     <Loading/>
                 ) : (
-                    <TodoList
-                        todoList={todoList}
-                        onRemoveTodo={removeTodo}
-                    />
+                    <>
+                        <SortBox
+                            isReversed={isReversed}
+                            onIsReversedChange={handleIsReversedChange}
+                            onSortFieldChange={handleSortFieldChange}
+                            sortField={sortBy}
+                         />
+                        <TodoList
+                            todoList={todoList}
+                            onRemoveTodo={removeTodo}
+                        />
+                    </>
                 )}
             </div>
         </div>
