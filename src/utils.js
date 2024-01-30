9f3adbf98e5a123177cc2ad493942b1445f2da25
Cameron Marr <alphaomegabeta@gmail.com>
2024-01-30 09:55:12 -0800

    Modify sortByField to create and return copy of array

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,6 @@
 export const sortByField = (array, fieldName, isReversed = false) => {
-    return array.sort((a, b) => {
+    const copy = [...array]
+    return copy.sort((a, b) => {
         const fieldA = a[fieldName];
         const fieldB = b[fieldName];
         
