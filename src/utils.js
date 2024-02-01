22c2a5d501ba7541109769433f0e1e3f9610247e
Cameron Marr <alphaomegabeta@gmail.com>
2024-02-01 12:46:13 -0800

    Modify sort to ignore case

--- src/utils.js
+++ src/utils.js
@@ -1,10 +1,10 @@
 export const sortByField = (array, fieldName, isReversed = false) => {
     const copy = [...array]
     return copy.sort((a, b) => {
-        const fieldA = a[fieldName];
-        const fieldB = b[fieldName];
+        const fieldA = a[fieldName].toLowerCase();
+        const fieldB = b[fieldName].toLowerCase();
         
-        if (fieldA.toLowerCase() > fieldB.toLowerCase()) {
+        if (fieldA > fieldB) {
             return isReversed ? -1 : 1;
         } else if (fieldA < fieldB) {
             return isReversed ? 1 : -1;
