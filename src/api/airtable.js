15e13c687badbee7f72d733f3b7fc8053b5d7d5c
Cameron Marr <alphaomegabeta@gmail.com>
2024-02-01 10:26:07 -0800

    Modify fetchAirtableData default url

--- src/api/airtable.js
+++ src/api/airtable.js
@@ -1,10 +1,7 @@
 import axios from "axios";
 
 const BASE_URL = "https://api.airtable.com/v0";
-const BASE_ID = process.env.REACT_APP_AIRTABLE_BASE_ID;
 const API_TOKEN = process.env.REACT_APP_AIRTABLE_API_TOKEN;
-
-const AIRTABLE_URL = `${BASE_URL}/${BASE_ID}`;
 
 export const fetchAirtableData = async ({ method, url, body }) => {
     const headers = {
@@ -14,7 +11,7 @@ export const fetchAirtableData = async ({ method, url, body }) => {
     
     const axiosConfig = {
         method: method,
-        url: `${AIRTABLE_URL}${url ?? ""}`,
+        url: `${BASE_URL}/${url}`,
         headers: headers,
         ...(body ? { data: body } : {})
     };
