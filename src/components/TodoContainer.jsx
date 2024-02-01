247b93fabf7df12c96233c84a57df32b2f0eb7af
Cameron Marr <alphaomegabeta@gmail.com>
2024-02-01 09:26:49 -0800

    Move fetchAirtableData to airtable.js

--- src/components/TodoContainer.jsx
+++ src/components/TodoContainer.jsx
@@ -1,48 +1,20 @@
 import React, { useEffect, useState } from "react";
-import axios from "axios";
 import styles from "../styles/TodoContainer.module.css";
 import AddTodoForm from "./AddTodoForm";
 import Loading from "./Loading";
 import TodoList from "./TodoList";
 import { sortByField } from "../utils";
 import SortBox from "./SortBox";
+import { fetchAirtableData } from "../api/airtable";
 
 const LOCAL_STORAGE_SORT_KEY = "todoListSort";
-
-const BASE_URL = "https://api.airtable.com/v0";
-const BASE_ID = process.env.REACT_APP_AIRTABLE_BASE_ID;
-const TABLE_NAME = process.env.REACT_APP_TABLE_NAME;
-const API_TOKEN = process.env.REACT_APP_AIRTABLE_API_TOKEN;
 const GRID_VIEW = "view=Grid view";
-
-const AIRTABLE_URL = `${BASE_URL}/${BASE_ID}/${TABLE_NAME}`;
-
-const fetchAirtableData = async ({ method, url, body }) => {
-    const headers = {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${API_TOKEN}`
-    };
-
-    const axiosConfig = {
-        method: method,
-        url: `${AIRTABLE_URL}${url ?? ""}`,
-        headers: headers,
-        ...(body ? { data: body } : {})
-    };
-
-    try {
-        const response = await axios(axiosConfig);
-        return response.data;
-    } catch (error) {
-        throw new Error(`Error: ${error.response ? error.response.status : "Unknown"}`);
-    }
-};
 
 const savedSortOptions = JSON.parse(localStorage.getItem(LOCAL_STORAGE_SORT_KEY)) ?? {};
 const initialSort = savedSortOptions.sortBy ?? "lastModifiedTime";
 const initialIsReversed = savedSortOptions.isReversed ?? true;
 
-const TodoContainer = () => {
+const TodoContainer = ({ tableName }) => {
     const [todoList, setTodoList] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
     const [sortBy, setSortBy] = useState(initialSort);
@@ -51,11 +23,12 @@ const TodoContainer = () => {
     const addTodo = async (newTodo) => {
         const airtableData = {
             fields: {
-                title: newTodo
+                title: newTodo,
+                createDate: new Date().toISOString()
             }
         };
         try {
-            await fetchAirtableData({ method: "POST", body: airtableData });
+            await fetchAirtableData({ method: "POST", body: airtableData, url: `/${tableName}` });
         } catch (error) {
             console.log(error.message);
         }
@@ -64,7 +37,7 @@ const TodoContainer = () => {
 
     const removeTodo = async (id) => {
         try {
-            await fetchAirtableData({ method: "DELETE", url: `/${id}` });
+            await fetchAirtableData({ method: "DELETE", url: `/${tableName}/${id}` });
         } catch (error) {
             console.log(error.message);
         }
@@ -73,7 +46,7 @@ const TodoContainer = () => {
 
     const loadTodos = async () => {
         try {
-            const response = await fetchAirtableData({ method: "GET", url: `?${GRID_VIEW}` });
+            const response = await fetchAirtableData({ method: "GET", url: `/${tableName}?${GRID_VIEW}` });
 
             const todosFromAPI = await response;
 
@@ -83,7 +56,8 @@ const TodoContainer = () => {
                         return {
                             id: todo.id,
                             title: todo.fields.title,
-                            lastModifiedTime: todo.fields.lastModifiedTime
+                            createDate: todo.fields.createDate,
+                            completeDate: todo.fields.completeDate
                         };
                     }),
                         initialSort,
@@ -101,7 +75,7 @@ const TodoContainer = () => {
             await loadTodos();
             setIsLoading(false);
         })();
-    }, []);
+    }, [tableName]);
 
     useEffect(() => {
         setTodoList(prevState => sortByField(prevState, sortBy, isReversed));
