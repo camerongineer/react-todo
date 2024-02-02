ea28d1e68b941fb1476e80590d374dfc9758485c
Cameron Marr <alphaomegabeta@gmail.com>
2024-02-02 12:32:40 -0800

    Add CreateContainer component

---
+++ src/components/CreateContainer.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { useNavigate, useSearchParams } from "react-router-dom";
+import { createNewTable } from "../api/airtable";
+import RedirectCreate from "./RedirectCreate";
+import RequestCreate from "./RequestCreate";
+
+const CreateContainer = () => {
+    const [params] = useSearchParams();
+    const tableName = params?.get("t");
+    const navigate = useNavigate();
+    
+    const handleCreateClicked = async (newName) => {
+        await createNewTable(newName.trim())
+        navigate(`/list/${newName}`);
+    }
+    
+    if (!tableName?.trim().length) return <RequestCreate onCreateClicked={handleCreateClicked}/>
+    
+    return <RedirectCreate
+        tableName={tableName}
+        onCreateClicked={handleCreateClicked
+    }/>;
+};
+
+export default CreateContainer;
