7b576dfd2d5e8ccf12516862e74d84b40d8797ce
Cameron Marr <alphaomegabeta@gmail.com>
2024-02-02 12:43:35 -0800

    Create shallow copy with Arrays.from; Refactor for more readable syntax; Convert fields to lower case before comparing to ignore case comparisons

--- src/utils.js
+++ src/utils.js
@@ -1,14 +1,13 @@
 export const sortByField = (array, fieldName, isReversed = false) => {
-    const copy = [...array]
+    const copy = Array.from(array)
     return copy.sort((a, b) => {
-        const fieldA = a[fieldName];
-        const fieldB = b[fieldName];
+        const fieldA = a[fieldName].toLowerCase();
+        const fieldB = b[fieldName].toLowerCase();
         
-        if (fieldA > fieldB) {
-            return isReversed ? -1 : 1;
-        } else if (fieldA < fieldB) {
-            return isReversed ? 1 : -1;
-        }
-        return 0;
+        if (fieldA === fieldB) return 0;
+        
+        if (fieldA > fieldB) return isReversed ? -1 : 1;
+        
+        return isReversed ? 1 : -1;
     });
 };
