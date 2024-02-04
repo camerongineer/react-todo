3c9c42ed4b60aeddac2a76986e71594f159179c2
Cameron Marr <alphaomegabeta@gmail.com>
2024-02-04 11:03:14 -0800

    Create homeLoader function

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,16 @@
 import React from "react";
 import TodoContainer from "./TodoContainer";
-import { useParams } from "react-router-dom";
+import { redirect, useParams } from "react-router-dom";
+import { createNewTable, getTableNames } from "../utils/fetchAirtableData";
+
+const homeLoader = async () => {
+    const tables = await getTableNames();
+    if (!tables.length || !tables.some(table => table.name === "Todo")) {
+        await createNewTable("Todo");
+        throw redirect("/list/Todo");
+    }
+    return tables;
+};
 
 const Home = () => {
     const { tableName } = useParams();
@@ -9,4 +19,5 @@ const Home = () => {
     );
 };
 
+export { homeLoader };
 export default Home;
