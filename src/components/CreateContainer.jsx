2269ca24b0a870afd2d2f13817a8490ed293069d
Cameron Marr <alphaomegabeta@gmail.com>
2024-02-10 11:57:26 -0800

    Modify for use with useNavigate hook

--- src/components/CreateContainer.jsx
+++ src/components/CreateContainer.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useSearchParams } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
 import { createNewTable } from "../utils/fetchAirtableData";
 import RedirectCreate from "./RedirectCreate";
 import RequestCreate from "./RequestCreate";
@@ -7,18 +7,19 @@ import RequestCreate from "./RequestCreate";
 const CreateContainer = () => {
     const [params] = useSearchParams();
     const tableName = params?.get("t");
+    const navigate = useNavigate();
     
     const handleCreateClicked = async (newName) => {
-        await createNewTable(newName.trim())
-        window.location.href = `/list/${newName}`;
-    }
+        await createNewTable(newName.trim());
+        navigate(`/list/${newName}`);
+    };
     
-    if (!tableName?.trim().length) return <RequestCreate onCreateClicked={handleCreateClicked}/>
+    if (!tableName?.trim().length) return <RequestCreate onCreateClicked={handleCreateClicked}/>;
     
     return <RedirectCreate
         tableName={tableName}
-        onCreateClicked={handleCreateClicked
-    }/>;
+        onCreateClicked={handleCreateClicked}
+    />;
 };
 
 export default CreateContainer;
