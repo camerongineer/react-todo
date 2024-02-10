a01246cb5c31ddc699e934b1dcc8a488727bae53
Cameron Marr <alphaomegabeta@gmail.com>
2024-02-10 11:51:27 -0800

    Add styling to button and layout

--- src/components/RequestCreate.jsx
+++ src/components/RequestCreate.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import InputWithLabel from "./InputWithLabel";
+import Header from "./Header";
+import styles from "../styles/RequestCreate.module.css";
 
 const RequestCreate = ({
     onCreateClicked
@@ -7,24 +9,33 @@ const RequestCreate = ({
     const [tableName, setTableName] = useState("");
     
     const handleSubmit = async (event) => {
-        event.preventDefault()
-        await onCreateClicked(tableName)
-    }
+        event.preventDefault();
+        await onCreateClicked(tableName);
+    };
     
     return (
-        <div>
+        <>
+            <Header tableName={tableName}/>
             <form
                 onSubmit={handleSubmit}
             >
-                <InputWithLabel
-                    inputId="newTableInput"
-                    inputValue={tableName}
-                    onInputChange={(event) => setTableName(event.target.value)}
-                    isFocused
-                />
-                <button type="submit">Create List</button>
+                <div className={styles.FormContainer}>
+                    <InputWithLabel
+                        inputId="newTableInput"
+                        inputValue={tableName}
+                        onInputChange={(event) => setTableName(event.target.value)}
+                        isFocused
+                    />
+                    <button
+                        type="submit"
+                        disabled={!tableName.length}
+                        className={!tableName.length ? styles.Disabled : styles.Button}
+                    >
+                        Create
+                    </button>
+                </div>
             </form>
-        </div>
+        </>
     );
 };
 
