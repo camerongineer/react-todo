b09c1ed5a352f8aff1367b5cfbc76d16c9982094
Cameron Marr <alphaomegabeta@gmail.com>
2024-02-10 11:56:34 -0800

    Add No button; Add preview header

--- src/components/RedirectCreate.jsx
+++ src/components/RedirectCreate.jsx
@@ -1,15 +1,24 @@
 import React from "react";
+import Header from "./Header";
+import { useNavigate } from "react-router-dom";
+import styles from "../styles/RedirectCreate.module.css";
 
 const RedirectCreate = ({
     tableName,
     onCreateClicked
 }) => {
-    const handleCreateClicked = async () => await onCreateClicked(tableName);
+    const navigate = useNavigate();
+    const handleYesClicked = async () => await onCreateClicked(tableName);
+    const handleNoClicked = async () => navigate("/");
     
     return (
         <div>
+            <Header tableName={tableName}/>
             <h3>"{tableName}" doesn't exist. Would you like create a list by this name?</h3>
-            <button onClick={handleCreateClicked}>Create {tableName} List</button>
+            <div className={styles.ButtonContainer}>
+                <button onClick={handleYesClicked}>Yes</button>
+                <button onClick={handleNoClicked}>No</button>
+            </div>
         </div>
     );
 };
